20051acb47188dc88191bdb00de4e9a68dd88a42
chirag-bajaj-creator <chiragbajaj2203@gmail.com>
2026-03-30 02:26:47 -0400

    done

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,3 +101,28 @@ class Enrollment(models.Model):
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
+class Question(models.Model):
+    # used to persist the questions for the course 
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question=models.CharField(max_length=200)
+    grade_point=models.FloatField(default=50)
+
+    def str(self):
+        return "Question:"+self.content
+    	# method to calculate if the learner gets the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice=models.CharField(max_length=200)
+    is_correct=models.BooleanField(default=False)
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
